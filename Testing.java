ecbab6f5c1d1f43949e815effd7ce5767e9ffa7e
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2022-06-29 13:03:38 -0500

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -49,39 +49,41 @@ public class Testing
     return index of largest element in array
     */    
     public static int findLargest(int[] arr, int start, int finish) {
+        System.out.println("Finding Largest:");
         int maxNum= -999;
         int maxIndex= -1;
-        for (int i=0; i>finish; i++) {
+        for (int i=start; i<finish; i++) {
             int num=arr[i];
             if(num>maxNum) {
                 maxNum=num;
                 maxIndex=i;
+                //System.out.println("new max num: "+ num);
             }
         }
         return maxIndex;
     }
 
-
     public static void main(String[] args) {
 
         //TEST1:
-		System.out.println("Testing Array1:");
-        int arr1[]={1,2,3, 6, 4};
-
+	System.out.println("Testing Array1:");
+        int arr1[]={6, 3, 2, 6, 4, 100};
         int len1=arr1.length;
-        int test1=search(arr1, 4, 0, len1);
-        int last1=arraySearch(arr1, 21, 0, len1);
+
+        int test1=search(arr1, 4, 0, len1);  
+        int last1=arraySearch(arr1, 3, 0, len1);
         int max1=findLargest(arr1, 0, len1 );
 
         System.out.println(test1);
         System.out.println(last1);
         System.out.println(max1);
 
-        int arr2[]={4, 13,21,30, 6, 4, 5};   
-
+      
         //TEST2:
-        System.out.println("Testing Array2:");
+        System.out.println("\nTesting Array2:");
+        int arr2[]={4, 13,21,30, 6, 4, 5};   
         int len2=arr1.length;
+        
         int test2=search(arr2, 30, 0, len2);
         int last2=arraySearch(arr2, 4, 0, len2);
         int max2=findLargest(arr2, 0, len2);
