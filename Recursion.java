89fbe6df7a9dac8533b51bd42de80c82740be108
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2022-07-19 15:39:00 -0500

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,30 +1,38 @@
-// Recursion; Data Structures; Scott Schmidt; Illinois State University
+//Recursion; Data Structures; Scott Schmidt; Illinois State University
 //https://code.sololearn.com/c4ZUSIu5EB6U
 //https://code.sololearn.com/cIkMAxQsNJ0E
 
 import java.util.*;
 import java.util.List;
 
- public class Recursion {
+public class Global {
    static boolean found=false;
- public static void main(String[] args) throws Exception {
-    int[] nums2 = {5, 2, 9};
-	  System.out.println("\nOriginal array: "+Arrays.toString(nums2));
-    List<List<Integer>> result2 = new Recursion().permute(nums2);
-	  System.out.println("\nPossible permutations of the said array:");
-	   // result2.forEach(System.out::println);	
-    getList(result2, 11);
+}
+
+public class Recursion {
+    public static void main(String[] args) throws Exception {
+        int[] nums3 = {7, 4, 3};
+        System.out.println("\nOriginal array: "+Arrays.toString(nums3));
+        List<List<Integer>> result3 = new Recursion().permute(nums3);
+        System.out.println("\nPossible permutations of the said array:");
+        getList(result3, 20);
+
+        int[] nums2 = {5, 2, 9};
+        System.out.println("\nOriginal array: "+Arrays.toString(nums2));
+        List<List<Integer>> result2 = new Recursion().permute(nums2);
+        System.out.println("\nPossible permutations of the said array:");
+        getList(result2, 11);  // result2.forEach(System.out::println);	
 	  }
 
     public static void getList(List<List<Integer>> lists, int find){
-      
     for (int i =0; i<lists.size(); i++) {
         List<Integer> list = lists.get(i); 
-        System.out.println(list);
-        findNum(list, find);
+        findNum(list, find); //System.out.println(list);
+        }
+        if (Global.found==false) {
+          System.out.println(find+ " not found.");
         }
     }
-
 
     public static void findNum(List<Integer> list, int find) {
       int sum=0;
@@ -32,8 +40,9 @@ import java.util.List;
       for (int i=0; i<list.size(); i++) {
         sum=sum+list.get(i);
         if (sum==find) {
+           System.out.println(list);
            System.out.println(sum + " sum found");
-           Recursion.found=true;
+           Global.found=true;
         }
       }
     }
@@ -65,22 +74,19 @@ import java.util.List;
 
 
 
-// Recursion; Scott Schmidt; Illinois State University
-// Java version: https://code.sololearn.com/cIkMAxQsNJ0E
+//Recursion; Data Structures; Scott Schmidt; Illinois State University
+//https://code.sololearn.com/c4ZUSIu5EB6U
+//https://code.sololearn.com/cIkMAxQsNJ0E
+
+import java.util.*; 
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.List;
 
-//The first argument will be the desired sum. 
-//Print the list of numbers that add up to the desired sum to the screen.
-//If no such set of numbers exists, print the message: "Sum not found." Call your file Sum.java.
-
-
-//Instead of an array of characters, read the input as a string. 
-//Print the resulting sequences to the screen. 
-//Call your file Combinations.java.
-
-class Permutation {
+class p10 {
+    //Instead of an array of characters, read the input as a string. 
+    //Print the resulting sequences to the screen. 
+    //Call your file Combinations.java.
     private static List<String> permutation(String prefix, String str) {
         List<String> permutations = new ArrayList<>();
         int n = str.length();
@@ -101,7 +107,6 @@ class Permutation {
         for (int i = 0; i < perms.size(); i++) {
             array[i] = perms.get(i);
         }
-
         int x = array.length;
 
         for (final String anArray : array) {
